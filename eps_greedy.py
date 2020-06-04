83f6298e6339a897887b7bd30efe3d29f92c8509
Nizamuddin <nizamphoenix@gmail.com>
2020-06-04 21:21:41 +1000

    Update eps_greedy.py

--- eps_greedy.py
+++ eps_greedy.py
@@ -2,8 +2,7 @@ from mab import MAB
 
 class EpsGreedy(MAB):
     """
-    Epsilon-Greedy multi-armed bandit
-
+    Epsilon-greedy is gullible when it is presented with good rewards.
     Arguments
     =========
     narms : int
@@ -23,22 +22,24 @@ class EpsGreedy(MAB):
         self.Qvalues = np.full((narms,),Q0)#use infinity
     
 
-    #This returns an arm integer in {1, . . . , self.narms}
+
     def play(self, tround, context=None):
+        #This returns an arm integer in {1, . . . , self.narms}
         if np.random.random() > self.epsilon:#flipping a coin and chosing an arm at random
             return np.random.choice([i+1 for i, j in enumerate(self.Qvalues) if j == max(self.Qvalues)])
                    #Return the index(arm number) of the arm with the largest Q-value;in case of ties(more than 1 max value) pick a random index
         else:
             return 1+np.random.choice(range(len(self.Qvalues)))#chosing an arm based on calulated Qvalues
 
-    """
-    1.Increment the counts field that records the number of times we’ve played each of the arms.
-    2.Find the current estimated value of the chosen arm.
-      a)If this is our first experience with the chosen arm, we set the estimated value directly.
-      b)If we had played the arm in the past, we update the estimated value of the chosen arm to be a weighted average of the
-        previously estimated value and the reward we just received
-    """
+    
     def update(self, arm, reward, context=None):
+        """
+        1.Increment the counts field that records the number of times we’ve played each of the arms.
+        2.Find the current estimated value of the chosen arm.
+          a)If this is our first experience with the chosen arm, we set the estimated value directly.
+          b)If we had played the arm in the past, we update the estimated value of the chosen arm to be a weighted average of the
+            previously estimated value and the reward we just received
+        """
         self.counts_of_arms[arm-1] += 1
         n = self.counts_of_arms[arm-1]
         if self.Qvalues[arm-1]==np.inf:#First experience with the arm.
@@ -48,4 +49,3 @@ class EpsGreedy(MAB):
         new_value = value * ((n - 1) / float(n)) + reward / float(n)
         self.Qvalues[arm-1] = new_value
 
-#Epsilon-greedy is gullible when it is presented with arms with good rewards.
